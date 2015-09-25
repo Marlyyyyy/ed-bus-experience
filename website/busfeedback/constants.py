37e859e34f1eefafd507a42a22f3ceeebbf0eaae
Marton Szeles <s1244474@sms.ed.ac.uk>
2015-09-25 22:11:30 +0100

    [website] Added "bus_updater" utility which is responsible for populating the Stop and Service tables.

---
+++ website/busfeedback/constants.py
@@ -0,0 +1,9 @@
+# Bus API constants
+API_HEADER = {'Authorization': 'Token 0c627af5849e23b0b030bc7352550884'}
+API_STOPS = 'https://tfe-opendata.com/api/v1/stops'
+API_SERVICES = 'https://tfe-opendata.com/api/v1/services'
+API_VEHICLES = 'https://tfe-opendata.com/api/v1/vehicle_locations'
+
+STOPS_UPDATE_RATE = 86400000
+SERVICES_UPDATE_RATE = 86400000
+VEHICLES_UPDATE_RATE = 20000
---
+++ website/busfeedback/utilities/bus_updater.py
@@ -0,0 +1,109 @@
+from busfeedback.models.update import Update
+from busfeedback.models.service import Service
+from busfeedback.models.stop import Stop
+import requests
+from busfeedback.constants import *
+import json
+from datetime import datetime
+
+
+def is_table_filled(table_name):
+    return Update.objects.filter(table_name=table_name).exists()
+
+
+# Updates the date corresponding to when a specific table was last updated
+def update_table_date(table_name, new_date):
+    if is_table_filled(table_name):
+        update = Update.objects.filter(table_name=table_name)[0]
+        update.last_updated = new_date
+    else:
+        update = Update(table_name=table_name, last_updated=new_date)
+
+    update.save()
+
+
+# Returns True if the selected table requires to be updated. Parameter "difference" has to be milliseconds.
+def check_if_need_update(table_name, current_date, difference):
+    if is_table_filled(table_name):
+        update = Update.objects.filter(table_name=table_name)[0]
+        delta = current_date - update.last_updated
+        actual_difference = int(delta.total_seconds() * 1000)
+        if actual_difference > difference:
+            return True
+        else:
+            return False
+    else:
+        return True
+
+
+# Deletes existing data from stops, services and their linking table
+def delete_services_stops():
+    Service.query.delete()
+    Stop.query.delete()
+
+
+# Fills up both services and stops with up-to-date data
+def update_services_and_stops():
+
+    # Get services as json data
+    services_json = requests.get(API_SERVICES, headers=API_HEADER)
+    services_dictionary = json.loads(services_json.text)
+
+    # Array of service objects
+    services = services_dictionary["services"]
+
+    # Get stops as json data
+    stops_json = requests.get(API_STOPS, headers=API_HEADER)
+    stops_dictionary = json.loads(stops_json.text)
+
+    # Array of stop objects
+    stops = stops_dictionary["stops"]
+
+    # Update the tables' last_updated field
+    service_date = datetime.utcfromtimestamp(int(services_dictionary["last_updated"]))
+    update_table_date("service", service_date)
+    stop_date = datetime.utcfromtimestamp(int(stops_dictionary["last_updated"]))
+    update_table_date("stop", stop_date)
+
+    # Create a dictionary of stopID,Services
+    stop_services_dictionary = {}
+    for stop in stops:
+        # Must take the set because sometimes the service names are listed multiple times for a particular stop.
+        service_names = set(stop["services"])
+        stop_services_dictionary[stop["stop_id"]] = service_names
+
+    # Create a service instance of each service
+    new_services = []
+    for service in services:
+        new_services.append(Service(name=service["name"], type=service["service_type"], description=service["description"]))
+
+    # Create a stop instance of each stop and add corresponding services to them
+    new_stops = []
+    for stop in stops:
+        new_stops.append(Stop(name=stop["stop_id"], latitude=stop["latitude"], longitude=stop["longitude"]))
+
+    # Insert all services using a single query
+    Service.objects.bulk_create(new_services)
+
+    # Insert all stops using a single query
+    Stop.objects.bulk_create(new_stops)
+
+    # Manage the relationship between Service and Stop
+    saved_stops = Stop.objects.all()
+    saved_services = Service.objects.all()
+
+    saved_services_dictionary = {}
+    for service in saved_services:
+        saved_services_dictionary[service.name] = service
+
+    # Add services to stops
+    for stop in saved_stops:
+        service_names_to_add = stop_services_dictionary[stop.name]
+        services_to_add = []
+        for service_name in service_names_to_add:
+            services_to_add.append(saved_services_dictionary[service_name])
+        stop.services.add(*services_to_add)
+        stop.save()
+
+
+
