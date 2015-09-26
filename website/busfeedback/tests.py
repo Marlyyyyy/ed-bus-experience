8ffcb349997e85ca0735019160b6fc68f6d4885b
Marton Szeles <s1244474@sms.ed.ac.uk>
2015-09-26 13:23:39 +0100

    [website] Added test case for updating services and stops.

--- website/busfeedback/tests.py
+++ website/busfeedback/tests.py
@@ -3,12 +3,13 @@ from busfeedback.utilities.bus_updater import *
 from busfeedback.constants import *
 from datetime import datetime, timedelta
 from django.utils import timezone
+from busfeedback.models.service import Service
+from busfeedback.models.stop import Stop
 
 
 class BusUpdaterTestCase(TestCase):
 
     def test_check_if_need_update(self):
-
         update_table_date("services_table", timezone.now())
 
         need_for_update = check_if_need_update("services_table", timezone.now(), SERVICES_UPDATE_RATE)
@@ -19,3 +20,18 @@ class BusUpdaterTestCase(TestCase):
 
         need_for_update = check_if_need_update("services_table", timezone.now()+timedelta(days=1), SERVICES_UPDATE_RATE)
         self.assertTrue(need_for_update, "Needs an update 1 day after inserting.")
+
+    def test_update_services_and_stops(self):
+        services = Service.objects.all()
+        self.assertFalse(services, "Services should be empty.")
+
+        stops = Stop.objects.all()
+        self.assertFalse(stops, "Stops should be empty.")
+
+        update_services_and_stops()
+
+        services = Service.objects.all()
+        self.assertGreater(services.count(), 0, "Services should be empty.")
+
+        first_service_stops = services[0].stops
+        self.assertGreater(first_service_stops.count(), 0, "Stops should be empty.")
