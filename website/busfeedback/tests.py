7f1874f178156b70d53e9ec27703075f7b294cbc
Marton Szeles <s1244474@sms.ed.ac.uk>
2015-09-26 12:48:53 +0100

    [website] Added backend test for checking whether the Service and Stop tables need to be updated.

--- website/busfeedback/tests.py
+++ website/busfeedback/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from busfeedback.utilities.bus_updater import *
+from busfeedback.constants import *
+from datetime import datetime, timedelta
+from django.utils import timezone
 
-# Create your tests here.
+
+class BusUpdaterTestCase(TestCase):
+
+    def test_check_if_need_update(self):
+
+        update_table_date("services_table", timezone.now())
+
+        need_for_update = check_if_need_update("services_table", timezone.now(), SERVICES_UPDATE_RATE)
+        self.assertFalse(need_for_update, "Does not need update immediately after inserting.")
+
+        need_for_update = check_if_need_update("services_table", timezone.now()+timedelta(hours=1), SERVICES_UPDATE_RATE)
+        self.assertFalse(need_for_update, "Does not need update 1 hour after inserting.")
+
+        need_for_update = check_if_need_update("services_table", timezone.now()+timedelta(days=1), SERVICES_UPDATE_RATE)
+        self.assertTrue(need_for_update, "Needs an update 1 day after inserting.")
